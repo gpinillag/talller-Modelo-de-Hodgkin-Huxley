f47b5b36afdef7020bbcc16de622d338653e0750
gpinillag <163017887+gpinillag@users.noreply.github.com>
2024-05-11 02:20:08 -0500

    Update taller_modeloHH.m

--- taller_modeloHH.m
+++ taller_modeloHH.m
@@ -2,39 +2,46 @@ clc
 
 C_m = 1;     
 g_Na = 120;   
-E_Na = 115;   
+E_Na = 50;   
 g_K = 36;     
-E_K = -12;    
-g_L = 0.3;    
-E_L = 10.6;   
+E_K = -77;    
+g_F = 0.3;    
+E_F = -54.4;   
+
 
 dt = 0.01;                 
 T = 50;                     
 t = 0:dt:T;                 
-num_steps = numel(t);
-V = zeros(1, num_steps);    
-m = zeros(1, num_steps);    
-n = zeros(1, num_steps);    
-h = zeros(1, num_steps);    
+pasos = numel(t);
 
-% Condiciones iniciales
-V(1) = -65;     
-m(1) = 0.5;     
-h(1) = 0.06;    
-n(1) = 0.5;     
+V = zeros(size(t));  
+m = zeros(size(t));  
+h = zeros(size(t));  
+n = zeros(size(t)); 
+V(1) = -65;
 
+x=input("Inicio de la corriente: ");
+    y=input("Fin de la corriente: ");
+    z=input("Cantidad de corriente aplicada (uA): ");
 
-for i = 1:num_steps-1
+    I = zeros(size(t));  
+    I(find(t >= x & t <= y)) = z;  
 
-    alpha_m = 0.1 * (V(i) + 40) / (1 - exp(-(V(i) + 40) / 10));
-    beta_m = 4 * exp(-(V(i) + 65) / 18);
-    alpha_h = 0.07 * exp(-(V(i) + 65) / 20);
-    beta_h = 1 / (1 + exp(-(V(i) + 35) / 10));
-    alpha_n = 0.01 * (V(i) + 55) / (1 - exp(-(V(i) + 55) / 10));
-    beta_n = 0.125 * exp(-(V(i) + 65) / 80);
+for i = 1:pasos-1
+
+alpha_m = (0.1*(V(i)+40)) / (1 - exp(-(V(i)+40)/10));
+beta_m = 4 * exp(-(V(i)+65)/18);
+alpha_h = 0.07 * exp(-(V(i)+65)/20);
+beta_h = 1 / (1 + exp(-(V(i)+35)/10));
+alpha_n = (0.01*(V(i)+55)) / (1 - exp(-(V(i)+55)/10));
+beta_n = 0.125 * exp(-(V(i)+65)/80);
     
-    
-    dvdt = (1/C_m) * (-g_Na * m(i)^3 * h(i) * (V(i) - E_Na) - g_K * n(i)^4 * (V(i) - E_K) - g_L * (V(i) - E_L));
+
+    iNa = g_Na * m(i)^3 * h(i) * (V(i) - E_Na);
+    iK = g_K * n(i)^4 * (V(i) - E_K);
+    iF = g_F * (V(i) - E_F);
+     
+    dvdt = (1/C_m) * (I(i)-iNa - iK - iF);
     dmdt = alpha_m * (1 - m(i)) - beta_m * m(i);
     dndt = alpha_n * (1 - n(i)) - beta_n * n(i);
     dhdt = alpha_h * (1 - h(i)) - beta_h * h(i);
@@ -44,6 +51,7 @@ for i = 1:num_steps-1
     m(i+1) = m(i) + dt * dmdt;
     n(i+1) = n(i) + dt * dndt;
     h(i+1) = h(i) + dt * dhdt;
+    
 end
 
 
@@ -53,6 +61,4 @@ xlabel('Tiempo (ms)');
 ylabel('Potencial de membrana (mV)');
 title('Modelo de Hodgkin-Huxley PA');
 grid on;
-
-
 
